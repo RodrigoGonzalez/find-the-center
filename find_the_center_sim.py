71836e02041f3be0ae616f20ef18c5e4b21ee541
Matthew Haigh <matthew.haigh@bonsai.ai>
2017-03-23 15:34:20 -0700

    Updating to release 2017.03.15-1

--- find_the_center_sim.py
+++ find_the_center_sim.py
@@ -1,85 +1,87 @@
 """ This Basic simulator is for learning the simulator interface.
 It can be used in this case to find the center between two numbers.
 """
+from __future__ import print_function
 import bonsai
 import sys
 from bonsai.simulator import SimState
 from random import randint
 
-#def debug(*args):
-    #print(*args, file=sys.stderr)
+""" If you would like to debug your code add this back in.
+def debug(*args):
+    print(*args, file=sys.stderr)
+"""
+
 
 class BasicSimulator(bonsai.Simulator):
-    """ A basic simulator class that takes in a move from the inkling file, and returns the state as a result of that move.
+    """ A basic simulator class that takes in a move from the inkling file,
+    and returns the state as a result of that move.
     """
+
     min = 0
     max = 2
     goal = 1
 
     def __init__(self):
-        super(self.__class__, self).__init__()
+        super(BasicSimulator, self).__init__()
         self.goal_count = 0
         self.value = randint(self.min, self.max)
         self.old_value = self.min
 
     def get_terminal(self):
-        """ Function to restart the simulation if the Inkling move was out of bounds.
+        """ Restarts the simulation if the AI moved out of bounds.
         """
-        if (self.value < self.min or
-            self.value > self.max): # or
-            #(self.value == self.goal and self.old_value == self.goal)):
+        if (self.value < self.min or self.value > self.max):
+            # (self.value == self.goal and self.old_value == self.goal)):
 
-            #debug("terminal")
+            # debug("terminal")
             self.reset()
             return True
         else:
             return False
 
     def start(self):
-        """ Function to start the episode by guessing a random integer between the min and max.
+        """ Start the episode by initializing value to a random number
+        between the min and max.
         """
-        #debug("start")
+        # debug("start")
         self.goal_count = 0
         self.old_value = self.min
         self.value = randint(self.min, self.max)
 
     def stop(self):
-        """ Function to stop the simulator.
+        """ Stop is called after a training session is complete.
+        This simple game requires no cleanup after training.
         """
-        #debug("stop")
+        # debug("stop")
         pass
 
     def reset(self):
-        """ Function to reset the simulation variables.
+        """ Reset is called to reset simulator state before the next training session.
         """
-        #debug("reset")
+        # debug("reset")
         self.goal_count = 0
         self.old_value = self.min
         self.value = randint(self.min, self.max)
 
     def advance(self, actions):
-        """ Function to make a move based on input from Inkling file and if in the center,
-        increases the total goal count by 1.
+        """ Function to make a move based on output from the BRAIN as defined
+        in the Inkling file.
         """
-        #debug("advance", actions["delta"])
+        # debug("advance", actions["delta"])
         self.value += actions["delta"]
         if self.value == self.goal:
             self.goal_count += 1
 
     def get_state(self):
-        """ Gets the state of the simulator, whether it be a valid value or terminal.
+        """ Gets the state of the simulator, whether it be a valid value or
+        terminal ("game over") state.
         """
-        #debug("get_state")
-        #debug("state", self.value)
+        # debug("get_state")
+        # debug("state", self.value)
         self.old_value = self.value
-        return SimState(state={"value": self.value},is_terminal=self.get_terminal())
-
-    def distance_from_goal(self):
-        """ Function to determine how far away the move is from the center.
-        """
-        dist = abs(self.goal - self.value)
-        #debug("dist", dist)
-        return -1*dist
+        return SimState(state={"value": self.value},
+                        is_terminal=self.get_terminal())
 
     def time_at_goal(self):
         """ Function to return how long the simulation has maintained the goal.
@@ -89,4 +91,5 @@ class BasicSimulator(bonsai.Simulator):
 if __name__ == "__main__":
     base_args = bonsai.parse_base_arguments()
     sim = BasicSimulator()
-    bonsai.run_with_url('find_the_center_sim', sim, base_args.brain_url, base_args.access_key)
+    bonsai.run_with_url('find_the_center_sim', sim,
+                        base_args.brain_url, base_args.access_key)
